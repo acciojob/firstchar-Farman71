b87d127bdafe1e67be95ed6a40409501d5cfa40b
Farman71 <farmanhussain71@gmail.com>
2025-01-20 13:20:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function firstChar(text) {
-    // your code here
-      let string=text.trim();
-      return string.length>0 ? string.charAt(0): "' '";
-      }
-  // Do not change the code below
-  //Uncomment the following line to show the prompt popup
-  const text = prompt("Enter text:");
-  alert(firstChar(text));
+    // Trim the string to remove spaces from the beginning and end
+    let string = text.trim();
+    // Return the first character or an empty string if there's nothing after trimming
+    return string.length > 0 ? string.charAt(0) : '';
+}
+
+// Do not change the code below
+// Uncomment the following line to show the prompt popup
+const text = prompt("Enter text:");
+alert(firstChar(text));
