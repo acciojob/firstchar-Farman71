068ee0a3d43a2fbd860fb13a0c6836277d4f6ed3
Farman71 <farmanhussain71@gmail.com>
2025-01-20 13:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
-  // your code here
-}
-
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+    // your code here
+      let string=text.trim();
+      return string.length>0 ? string.charAt(0): "' '";
+      }
+  // Do not change the code below
+  //Uncomment the following line to show the prompt popup
+  const text = prompt("Enter text:");
+  alert(firstChar(text));
